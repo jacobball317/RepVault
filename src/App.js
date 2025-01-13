111b6450d1b708b0d9086823f895cf01ccd3710d
Ball, Jacob Christopher <jacobball317@gmail.com>
2025-01-13 09:40:48 -0500

    Rid of CircuitList

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CircuitForm from "./components/CircuitForm";
-import CircuitList from "./components/CircuitList";
 import "./App.css";
 
 function App() {
-  const [circuits, setCircuits] = useState([
-    {
-      name: "Morning Routine",
-      restTime: 2,
-      cards: [
-        [
-          { name: "Push Ups", reps: 10, weight: 0, done: false },
-          { name: "Squats", reps: 15, weight: 0, done: false },
-        ],
-      ],
-    },
-    {
-      name: "Evening Routine",
-      restTime: 3,
-      cards: [
-        [
-          { name: "Pull Ups", reps: 5, weight: 0, done: false },
-          { name: "Deadlift", reps: 10, weight: 100, done: false },
-        ],
-      ],
-    },
-  ]); // List of circuits
   const [darkMode, setDarkMode] = useState(false); // Dark mode state
-
-  // Add a new circuit
-  const addCircuit = (newCircuit) => {
-    setCircuits([...circuits, newCircuit]);
-  };
 
   // Toggle dark mode
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
-
-  
 
   return (
     <Router>
@@ -68,14 +38,6 @@ function App() {
                       Create Circuit
                     </Link>
                   </button>
-                  <button>
-                    <Link
-                      to="/view-circuits"
-                      style={{ textDecoration: "none", color: "white" }}
-                    >
-                      View Circuits
-                    </Link>
-                  </button>
                 </nav>
               </div>
             }
@@ -86,19 +48,9 @@ function App() {
             path="/create-circuit"
             element={
               <CircuitForm
-                addCircuit={addCircuit}
-                totalCircuits={circuits.length}
                 darkMode={darkMode}
                 toggleDarkMode={toggleDarkMode}
               />
-            }
-          />
-
-          {/* View Circuits Page */}
-          <Route
-            path="/view-circuits"
-            element={
-              <CircuitList circuits={circuits} darkMode={darkMode} />
             }
           />
         </Routes>
@@ -110,7 +62,6 @@ function App() {
 if (module.hot) {
   module.hot.accept();
 }
-
 
 console.log("Home Screen Rendered");
 
